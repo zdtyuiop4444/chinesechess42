936aae404dc1fb5b6b0e9c69978a92ca5810c7be
imcu <11810922@mail.sustech.edu.cn>
2019-12-03 21:50:05 +0800

    want to use em

--- src/chinesechess/board/chessboardController.java
+++ src/chinesechess/board/chessboardController.java
@@ -1,25 +1,39 @@
 package chinesechess.board;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 
 public class chessboardController {
     @FXML
     public Circle carbli;
-
-    public chessboardController(){
-
-    }
-
+    @FXML
+    private BorderPane root;
     @FXML
     public GridPane gameboard;
     @FXML
     private Circle carbl;
+
+    private DoubleProperty em = new SimpleDoubleProperty(80);
+
     @FXML
     private void initialize() {
-
+        root.heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                em.set(newValue.doubleValue()/10);
+            }
+        });
     }
 
+    public chessboardController(){
+
+    }
 }
