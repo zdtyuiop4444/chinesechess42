dcf8072631ee9e1b440d2f85739bd744d9cde99c
imcu <11810922@mail.sustech.edu.cn>
2019-12-04 22:58:44 +0800

    Finish controller construcion

--- src/chinesechess/board/chessboardController.java
+++ src/chinesechess/board/chessboardController.java
@@ -7,18 +7,147 @@ import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.TextFlow;
 
 public class chessboardController {
     @FXML
-    private BorderPane root;
+    private Group Advisorbl;
+
     @FXML
-    public GridPane gameboard;
+    private Group Cannonbl;
+
     @FXML
     private Group Chariotbl;
+
+    @FXML
+    private Group Elephantbl;
+
+    @FXML
+    private Group Advisorrr;
+
+    @FXML
+    private Group Soldierrll;
+
+    @FXML
+    private Group Soldierrrr;
+
+    @FXML
+    private Group Chariotbr;
+
+    @FXML
+    private Group Elephantrr;
+
+    @FXML
+    private Group Cannonbr;
+
+    @FXML
+    private Group Horserl;
+
+    @FXML
+    private Group Soldierrl;
+
+    @FXML
+    private Group Elephantrl;
+
+    @FXML
+    private BorderPane root;
+
+    @FXML
+    private Group Generalb;
+
+    @FXML
+    private Group Advisorbr;
+
+    @FXML
+    private Group Soldierbll;
+
+    @FXML
+    private Group Soldierbrr;
+
+    @FXML
+    private Group Horserr;
+
+    @FXML
+    private Group Soldierrc;
+
+    @FXML
+    private GridPane gameboard;
+
+    @FXML
+    private Group Horsebl;
+
+    @FXML
+    private Group Soldierbl;
+
+    @FXML
+    private Group Generalr;
+
+    @FXML
+    private Group Horsebr;
+
+    @FXML
+    private Group Soldierbc;
+
+    @FXML
+    private Group Soldierrr;
+
+    @FXML
+    private Group Advisorrl;
+
+    @FXML
+    private Group Cannonrl;
+
+    @FXML
+    private Group Chariotrl;
+
+    @FXML
+    private Group Elephantbr;
+
+    @FXML
+    private Group Chariotrr;
+
+    @FXML
+    private Group Soldierbr;
+
+    @FXML
+    private Group Cannonrr;
+
+    @FXML
+    private Button savebutton;
+
+    @FXML
+    private Button loadbutton;
+
+    @FXML
+    private Button regretbutton;
+
+    @FXML
+    private Button defeatbutton;
+
+    @FXML
+    private Button sendbutton;
+
+    @FXML
+    private TextArea loglist;
+
+    @FXML
+    private TextArea inputarea;
+
+    @FXML
+    private TextFlow turnshower;
+
+    @FXML
+    private FlowPane bdead;
+
+    @FXML
+    private FlowPane rdead;
 
     @FXML
     private void initialize() {
